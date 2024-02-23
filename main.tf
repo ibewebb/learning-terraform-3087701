8b7fe17b57969d72f5cad6c2608b39add14b06f3
Chris Webb <103400231+ibewebb@users.noreply.github.com>
2024-02-23 14:34:48 -0700

    changed block to blocks

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ type       = "ingress"
 from_port  = 80
 to_port    = 80
 protocol   = "tcp"
-cidr_block = ["0.0.0.0/0"] 
+cidr_blocks = ["0.0.0.0/0"] 
 
 security_group_id = aws_security_group.blog.id
 }
@@ -51,7 +51,7 @@ type       = "ingress"
 from_port  = 443
 to_port    = 443
 protocol   = "tcp"
-cidr_block = ["0.0.0.0/0"] 
+cidr_blocks = ["0.0.0.0/0"] 
 
 security_group_id = aws_security_group.blog.id
 }
@@ -61,7 +61,7 @@ type       = "egress"
 from_port  = 0
 to_port    = 0
 protocol   = "-1"
-cidr_block = ["0.0.0.0/0"] 
+cidr_blocks = ["0.0.0.0/0"] 
 
 security_group_id = aws_security_group.blog.id
 }
