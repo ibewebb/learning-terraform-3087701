b8a48449da888bb3017f8e9b44b7bb5e3efe5984
Chris Webb <103400231+ibewebb@users.noreply.github.com>
2024-02-23 15:28:11 -0700

    Creating new dev VPC

--- main.tf
+++ main.tf
@@ -24,22 +24,22 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2", "us-west-2b", "us-west-2c"]
- 
+  azs             = ["us-west-2", "us-west-2b", "-west-1c"]
+  
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
   tags = {
     Terraform = "true"
     Environment = "dev"
   }
 }
 
-
-
 resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnet[0]
+  
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
