29f3b205a35991fbc04a21866c5faef0830b3fc1
Chris Webb <103400231+ibewebb@users.noreply.github.com>
2024-02-23 16:10:03 -0700

    changes attribute to list

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name            = "blog-alb"
@@ -48,13 +48,13 @@ module "alb" {
   subnets         = module.blog_vpc.public_subnets
   security_groups = module.blog_sg.security_group_id
 
-  http_tcp_listeners = {
+  http_tcp_listeners = [
     {
       port               = 80
       protocol           = "HTTP"
       target_group_index = 0
     }
-  }
+  ]
   
 
   target_groups = {
